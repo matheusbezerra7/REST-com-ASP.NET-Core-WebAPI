a0794095d38e31e361dcf174afc2995ce36dca1a
matheusbezerra7 <matheus.bezerra7@hotmail.com>
2021-04-29 15:14:07 -0300

    controller e views

---
+++ MinhaAPICompleta/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace DevIO.Api.Controllers
+{
+
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+
+        // validacao de notificacoes de erros 
+
+        //validacao de modelstate 
+
+        // validacao da operacao de negocios 
+
+    }
+}
